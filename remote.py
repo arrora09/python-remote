3e537c9f9d5c30c0b5badf10828ad146de4acae5
Tóth Áron Pál <ronyka1009@gmail.com>
2024-09-20 13:36:16 +0200

    gitignore, backend, and html frontend

---
+++ remote.py
@@ -0,0 +1,124 @@
+from flask import Flask, render_template, request
+import pyautogui
+from pynput.keyboard import Key, Controller
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+keyboard = Controller()
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+
+@app.route("/volUp", methods=["GET"])
+def volUp():
+    keyboard.press(Key.media_volume_up)
+    return "ok"
+
+
+@app.route("/volDown", methods=["GET"])
+def volDown():
+    keyboard.press(Key.media_volume_down)
+    return "ok"
+
+
+@app.route("/volMute", methods=["GET"])
+def volMute():
+    keyboard.press(Key.media_volume_mute)
+    return "ok"
+
+
+@app.route("/playpause", methods=["GET"])
+def playpause():
+    keyboard.press(Key.media_play_pause)
+    return "ok"
+
+
+@app.route("/up", methods=["GET"])
+def up():
+    keyboard.press(Key.up)
+    return "ok"
+
+
+@app.route("/down", methods=["GET"])
+def down():
+    keyboard.press(Key.down)
+    return "ok"
+
+
+@app.route("/left", methods=["GET"])
+def left():
+    keyboard.press(Key.left)
+    return "ok"
+
+
+@app.route("/right", methods=["GET"])
+def right():
+    keyboard.press(Key.right)
+
+    return "ok"
+
+
+@app.route("/typeText", methods=["POST"], strict_slashes=False)
+def typeText():
+    text = request.json["text"]
+    keyboard.type(text)
+    return "ok"
+
+
+@app.route("/enter", methods=["GET"])
+def enter():
+    keyboard.press(Key.enter)
+    return "ok"
+
+
+@app.route("/backspace", methods=["GET"])
+def backspace():
+    keyboard.press(Key.backspace)
+    return "ok"
+
+
+@app.route("/tab", methods=["GET"])
+def tab():
+    keyboard.press(Key.tab)
+    return "ok"
+
+
+@app.route("/skipForward", methods=["GET"])
+def skipForward():
+    keyboard.press(Key.media_next)
+    return "ok"
+
+
+@app.route("/skipBackward", methods=["GET"])
+def skipBackward():
+    keyboard.press(Key.media_previous)
+    return "ok"
+
+
+@app.route("/clickLeft", methods=["GET"])
+def clickLeft():
+    pyautogui.leftClick()
+    return "ok"
+
+
+@app.route("/clickRight", methods=["GET"])
+def clickRight():
+    pyautogui.rightClick()
+    return "ok"
+
+
+@app.route("/moveMouse", methods=["POST"], strict_slashes=False)
+def moveMouse():
+    x = request.json["x"]
+    y = request.json["y"]
+    oldX, oldY = pyautogui.position()
+    pyautogui.moveTo(oldX + (x * 30), oldY - (y * 30))
+    return "ok"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
